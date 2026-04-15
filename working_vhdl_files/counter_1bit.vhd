624892694d2b8fb13e50da1b46fe6cf2d0edef6b
OndraPotocek <o.potocek@gmail.com>
2026-04-15 14:28:43 +0200

    feat: architecture of counter_1bit

--- working_vhdl_files/counter_1bit.vhd
+++ working_vhdl_files/counter_1bit.vhd
@@ -10,5 +10,24 @@ Port(clk : in STD_LOGIC;
 end counter_1bit;
 
 architecture behavioral of counter_1bit is
-    
+
+begin
+    process(clk, rst)
+        variable count : integer := 0;
+    begin
+        if rst = '1' then
+            sig_digit <= '0';
+            count := 0;
+        elsif rising_edge(clk) then
+            if en = '1' then
+                if count = 0 then
+                    sig_digit <= '1';
+                    count := 1;
+                else
+                    sig_digit <= '0';
+                    count := 0;
+                end if;
+            end if;
+        end if;
+    end process;
 end behavioral;
