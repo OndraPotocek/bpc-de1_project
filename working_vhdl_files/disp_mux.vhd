8db8f5dac7f7158426cf3f0c68f6da60649cc0ca
OndraPotocek <o.potocek@gmail.com>
2026-04-15 22:24:00 +0200

    chore: renamed disp_mux output

--- working_vhdl_files/disp_mux.vhd
+++ working_vhdl_files/disp_mux.vhd
@@ -5,22 +5,22 @@ entity disp_mux is
     Port(sig_dig : in STD_LOGIC;
         sw0 : in STD_LOGIC_vector(3 downto 0);
         sw1 : in STD_LOGIC_VECTOR(1 downto 0);
-        sig_bin : out STD_LOGIC_VECTOR(3 downto 0);
+        bin : out STD_LOGIC_VECTOR(3 downto 0);
     );
 end disp_mux;
 
 architecture Behavioral of disp_mux is
 
 begin
-    process(sig_dig, sw0, sw1, sig_bin)
+    process(sig_dig, sw0, sw1, bin)
     begin
         switch (sig_dig) is
             when '0' =>
-                sig_bin <= sw0;
+                bin <= sw0;
             when '1' =>
-                sig_bin <= sw1 & "0000";
+                bin <= sw1 & "0000";
             when others =>
-                sig_bin <= "0000";
+                bin <= "0000";
         end switch;
     end process;
 end Behavioral;
