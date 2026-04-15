420254aeaf460b3ee9b251ebce2c273eb7fdd21d
OndraPotocek <o.potocek@gmail.com>
2026-04-15 14:57:02 +0200

    :feat architecture of disp_mux

--- working_vhdl_files/disp_mux.vhd
+++ working_vhdl_files/disp_mux.vhd
@@ -3,12 +3,24 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity disp_mux is
     Port(sig_dig : in STD_LOGIC;
-        sw0 : in STD_LOGIC;
-        sw1 : in STD_LOGIC;
-        sig_bin : in STD_LOGIC_VECTOR(3 downto 0);
+        sw0 : in STD_LOGIC_vector(3 downto 0);
+        sw1 : in STD_LOGIC_VECTOR(1 downto 0);
+        sig_bin : out STD_LOGIC_VECTOR(3 downto 0);
     );
 end disp_mux;
 
 architecture Behavioral of disp_mux is
 
+begin
+    process(sig_dig, sw0, sw1, sig_bin)
+    begin
+        switch (sig_dig) is
+            when '0' =>
+                sig_bin <= sw0;
+            when '1' =>
+                sig_bin <= sw1 & "0000";
+            when others =>
+                sig_bin <= "0000";
+        end switch;
+    end process;
 end Behavioral;
