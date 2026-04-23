f54b95fbd67a90c930e222b10540a796e7373e72
OndraPotocek <o.potocek@gmail.com>
2026-04-23 09:55:40 +0200

    test: counter1bit, sine_gen

---
+++ pwm/pwm.srcs/sim_1/new/counter_1bit_tb.vhd
@@ -0,0 +1,85 @@
+--! Testbench automatically generated online
+--! at https://vhdl.lapinoo.net
+--! Generation date : Wed, 15 Apr 2026 21:55:09 GMT
+--! Request id : cfwk-fed377c2-69e0093d0d0bb
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_counter_1bit is
+end tb_counter_1bit;
+
+architecture tb of tb_counter_1bit is
+
+    component counter_1bit
+        port (clk       : in std_logic;
+              rst       : in std_logic;
+              en        : in std_logic;
+              sig_digit : out std_logic);
+    end component;
+
+    signal clk       : std_logic;
+    signal rst       : std_logic;
+    signal en        : std_logic;
+    signal sig_digit : std_logic;
+
+    constant TbPeriod : time := 10 ns; -- ***EDIT*** Put right period here
+    signal TbClock : std_logic := '0';
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    dut : counter_1bit
+    port map (clk       => clk,
+              rst       => rst,
+              en        => en,
+              sig_digit => sig_digit);
+
+    -- Clock generation
+    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    -- ***EDIT*** Check that clk is really your main clock signal
+    clk <= TbClock;
+
+    stimuli : process
+    begin
+        -- ***EDIT*** Adapt initialization as needed
+        en <= '0';
+
+        -- Reset generation
+        -- ***EDIT*** Check that rst is really your reset signal
+        rst <= '1';
+        wait for 100 ns;
+        rst <= '0';
+        wait for 100 ns;
+        
+        en <= '1';
+        wait for 100 ns;
+        
+        en <= '0';
+        wait for 100 ns;
+        
+        en <= '1';
+        wait for 500 ns;
+        
+        en <= '0';
+        
+        wait for 300 ns;
+        
+        en <= '1';
+        
+        wait for 100 * TbPeriod;
+
+        -- Stop the clock and hence terminate the simulation
+        TbSimEnded <= '1';
+        wait;
+    end process;
+
+end tb;
+
+-- Configuration block below is required by some simulators. Usually no need to edit.
+
+configuration cfg_tb_counter_1bit of tb_counter_1bit is
+    for tb
+    end for;
+end cfg_tb_counter_1bit;
--- pwm/pwm.srcs/sources_1/imports/working_vhdl_files/disp_mux.vhd
+++ pwm/pwm.srcs/sources_1/imports/working_vhdl_files/disp_mux.vhd
@@ -21,13 +21,13 @@ end disp_mux;
 architecture Behavioral of disp_mux is
 
 begin
-    process(sig_dig, sw0, sw1)
+    process(sig_dig)
     begin
         case (sig_dig) is
             when '0' =>
                 bin <= sw0;
             when '1' =>
-                bin <= sw1 & "0000";
+                bin <= sw1 & "00";
             when others =>
                 bin <= "0000";
         end case;
